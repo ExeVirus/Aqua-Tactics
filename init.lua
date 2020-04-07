1e2196f87f560052aad4d5a9dc5661e3450239e7
ExeVirus <44562154+ExeVirus@users.noreply.github.com>
2020-04-07 18:13:09 -0400

    Create init.lua
    First Lua Version

---
+++ init.lua
@@ -0,0 +1,6 @@
+local settings = Settings(minetest.get_modpath('aqua-tactics')..'/settings.txt')
+local MP 	= minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP..'/intllib.lua')
+
+
+dofile(MP..'/fins.lua')
