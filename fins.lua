244355cb235a26aa8a4dcdc8d5a453db43b4eb73
ExeVirus <44562154+ExeVirus@users.noreply.github.com>
2020-04-07 19:04:47 -0400

    Update fins.lua

--- fins.lua
+++ fins.lua
@@ -2,6 +2,8 @@
 //When worn, this item makes the user faster underwater, 
 //...but slower out of the water.
 
-//It also has a configurable wear value, typically set to 200 seconds, feel free to modify as you see fit
+//It also has a configurable wear value, typically set to 300 seconds (5 minutes), feel free to modify as you see fit
+
+//Uses on-register globalstep to verify when you are on land or in water. 
 
 
