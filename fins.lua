25c107a687145b6ef4d4a759079c53d8d441cc56
ExeVirus <44562154+ExeVirus@users.noreply.github.com>
2020-04-07 18:15:23 -0400

    Create fins.lua
    First Version, just comments, haha

---
+++ fins.lua
@@ -0,0 +1,7 @@
+//Fins.lua implements the wearable item, fins. 
+//When worn, this item makes the user faster underwater, 
+//...but slower out of the water.
+
+//It also has a configurable wear value, typically set to 200 seconds, feel free to modify as you see fit
+
+
